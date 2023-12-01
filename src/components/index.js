95620e581814d4f1ffb3ee47a3810ade6673ecaf
David Out <david.out@migrato.nl>
2023-12-01 20:48:26 +0100

    Added useReducer to form

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,7 @@ export {default as Button} from './button/button.jsx';
 
 // input
 export * from './input';
+
+export {default as ProgressBar} from './progress-bar.jsx';
+export {default as LoadingDiv} from './loadingdiv.jsx';
+export {default as Logo} from './logo/logo.jsx';
--- src/pages/setup-account/form.jsx
+++ src/pages/setup-account/form.jsx
@@ -1,64 +1,91 @@
-import React, { useState } from 'react'
+import {default as useFormData } from './hooks/useFormData.jsx'
 
-import BStage1 from '@pages/setup-account/bedrijf/stage1.jsx'
-import BStage2 from '@pages/setup-account/bedrijf/stage2.jsx'
+import {Button, Logo, ProgressBar, InputField, OptionSelector} from "@components";
+import DefaultForm from './components/default-form.jsx';
+import {useState} from "react";
 
-import EStage1 from '@pages/setup-account/ervaringsdeskundige/stage1.jsx'
-import EStage2 from '@pages/setup-account/ervaringsdeskundige/stage2.jsx'
+function Form() {
+  const {formData, dispatch} = useFormData();
+  const {currentStage, maxStage} = formData;
 
-import Checkbox from "@components/input/checkbox.jsx";
-import DefaultForm from "@pages/setup-account/default-form.jsx";
-import InputField from "@components/input/inputfield.jsx";
-import OptionsSelector from "@components/input/optionselector.jsx";
-import Button from "@components/button/button.jsx";
+  const {callBack, form} = getNextForm(currentStage);
 
-
-export default function Form({moveOut, stage}) {
-    const [type, setType] = useState(null);
-    const cb = (type === 'Ervaringsdeskundige') ? <Checkbox>Ik ben ouder dan 18 jaar.</Checkbox> : <> </>;
-
-    console.log(stage);
-
-    if(type !== undefined && type !== null && stage > 0) {
-        switch (type) {
-            case 'Bedrijf':
-                return <BedrijfsForm stage={stage}/>
-            default:
-                return <ErvaringsdeskundigeForm stage={stage}/>
-        }
-    }
-    return (
-        <DefaultForm
-            moveOut={moveOut}
-            title='Account'
-        >
-            <InputField visible>Naam</InputField>
-            <InputField visible>Telefoonnummer</InputField>
-            <OptionsSelector onChange={(o,n) => setType(n)} options={['Ervaringsdeskundige', 'Bedrijf']}>Ik ben een</OptionsSelector>
-            {cb}
-            <Button onClick={nextStage}>Volgende</Button>
-        </DefaultForm>
-    );
-
+  return (
+      <>
+        <Logo id="logo"></Logo>
+        <ProgressBar stage={currentStage} maxStage={maxStage}/>
+          {form}
+        <Button onClick={callBack}>Volgende</Button>
+      </>
+  );
 }
 
-const ErvaringsdeskundigeForm = ({stage}) => {
-    switch (stage) {
-        case 2:
-            return <EStage2 stage={stage}/>
-        case 1:
-            return <EStage1 stage={stage}/>
-    }
+export default Form;
+
+function getNextForm(stage) {
+    const [data, setFormData] = useState({});
+
+    return {
+        callBack: () => {
+            console.log(data);
+        },
+        form:
+            <DefaultForm
+                title='Account'
+            >
+                <InputField visible onChange={(e) => data.username = e.target.value}>Naam</InputField>
+                <InputField visible onChange={(e) => data.phonenumber = e.target.value}>Telefoonnummer</InputField>
+                <OptionSelector onChange={(o,n) => data.type = n} options={['Ervaringsdeskundige', 'Bedrijf']}>Ik ben een</OptionSelector>
+            </DefaultForm>
+    };
 }
 
-const BedrijfsForm = ({stage}) => {
-    switch (stage) {
-        case 2:
-            return <BStage2 stage={stage}/>
-        case 1:
-            return <BStage1 stage={stage}/>
-    }
-}
+// export default function Form({moveOut, stage}) {
+//     const [type, setType] = useState(null);
+//     const cb = (type === 'Ervaringsdeskundige') ? <Checkbox>Ik ben ouder dan 18 jaar.</Checkbox> : <> </>;
+
+//     console.log(stage);
+
+//     if(type !== undefined && type !== null && stage > 0) {
+//         switch (type) {
+//             case 'Bedrijf':
+//                 return <BedrijfsForm stage={stage}/>
+//             default:
+//                 return <ErvaringsdeskundigeForm stage={stage}/>
+//         }
+//     }
+//     return (
+//         <DefaultForm
+//             moveOut={moveOut}
+//             title='Account'
+//         >
+//             <InputField visible>Naam</InputField>
+//             <InputField visible>Telefoonnummer</InputField>
+//             <OptionsSelector onChange={(o,n) => setType(n)} options={['Ervaringsdeskundige', 'Bedrijf']}>Ik ben een</OptionsSelector>
+//             {cb}
+//             <Button onClick={nextStage}>Volgende</Button>
+//         </DefaultForm>
+//     );
+
+// }
+
+// const ErvaringsdeskundigeForm = ({stage}) => {
+//     switch (stage) {
+//         case 2:
+//             return <EStage2 stage={stage}/>
+//         case 1:
+//             return <EStage1 stage={stage}/>
+//     }
+// }
+
+// const BedrijfsForm = ({stage}) => {
+//     switch (stage) {
+//         case 2:
+//             return <BStage2 stage={stage}/>
+//         case 1:
+//             return <BStage1 stage={stage}/>
+//     }
+// }
 
 
 

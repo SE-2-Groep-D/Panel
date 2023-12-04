6221287971b39196ce682ac82932966c5560d46a
David <71024003+DavidOut03@users.noreply.github.com>
2023-12-04 10:08:50 +0100

    Add server proxy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': 'https://localhost:3500',
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {

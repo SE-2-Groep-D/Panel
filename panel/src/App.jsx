25d4ac71152fefe73d5abfdeb211dc61523e1052
Ozancann01 <22008306@student.hhs.nl>
2024-01-02 17:12:31 +0100

    onderzoek formuleier aangemaakt

--- panel/src/App.jsx
+++ panel/src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useState, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import '@pagestyles/App.scss';
+
+import {SetupAccount} from "@pages";
+import OnderzoekForm from "@pages/research/onderzoekForm/onderzoekForm.jsx";
 // Lazy loading van pagina componenten
 const AlgemeneOnderzoek = React.lazy(() => import('@pages/research/AlgemeneOnderzoek.jsx'));
 const Onderzoek = React.lazy(() => import('@pages/research/Onderzoek.jsx'));
@@ -12,6 +15,8 @@ function App() {
                 <Routes>
                     <Route path="/onderzoek" element={<AlgemeneOnderzoek />} />
                     <Route path="/onderzoek/:onderzoekId" element={<Onderzoek />} />
+                    <Route path="/setup" element={<SetupAccount />} />
+                    <Route path="/onderzoek/aanmaken" element={<OnderzoekForm />} />
                 </Routes>
             </Suspense>
         </BrowserRouter>
---
+++ panel/src/pages/research/onderzoekForm/form/onderzoekStapTwee.jsx
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+
+import {Form, InputField} from "@components";
+import {useForm} from '../data/useForm.jsx';
+
+function OnderzoekStapTwee() {
+    const {state, nextStep, prevStep } = useForm();
+    const [message, setMessage] = useState(null);
+    const [move, setMove] = useState('moveIn');
+
+
+    const [values, setValues] = useState({
+        aantalParticipanten: '',
+        vergoeding: '',
+        datum: '',
+        websiteUrl: '',
+    });
+
+    function handleChange({element, value, id}) {
+        setValues({...values, [(id) ? id : element.id] : value});
+    }
+
+
+
+    function handleSubmit(formData) {
+        const {values} = formData;
+
+
+        state.onderzoek = values;
+
+    }
+
+    return (
+        <div>
+            <Form title="Onderzoek aanmaken" buttonText='volgende'  onSubmit={handleSubmit} move={move}>
+                <InputField id='aantalParticipanten' value={values.aantalParticipanten} onChange={handleChange} required>Aantal Participanten</InputField>
+                <InputField id='vergoeding' value={values.vergoeding} onChange={handleChange} required>Vergoeding</InputField>
+                <InputField id='datum' value={values.datum} onChange={handleChange} required>Datum</InputField>
+                <InputField id='websiteUrl' value={values.websiteUrl} onChange={handleChange} required>WebsiteUrl</InputField>
+            </Form>
+        </div>
+    );
+}
+
+
+export default OnderzoekStapTwee;
+

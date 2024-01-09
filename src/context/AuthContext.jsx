27193893b5fc9e54a2abcfebe3d7f66dac49e896
Jason <jason256709@outlook.com>
2024-01-09 23:41:08 +0100

    refresh

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,35 +1,51 @@
-import { createContext, useState } from 'react'
-
+import { createContext, useEffect, useState } from "react";
+import { fetchData } from "@api";
 
 export const AuthContext = createContext();
 
 const defaultAuth = {
-    authenticated: false,
-    userInfo: null,
-}
+  authenticated: false,
+  userInfo: null,
+};
 
-export function AuthProvider({children}) {
-    const [auth, setAuth] = useState(defaultAuth);
+export function AuthProvider({ children }) {
+  const [auth, setAuth] = useState(defaultAuth);
 
-    function loginUser(userId, userInfo) {
-        setAuth({
-            authenticated: true,
-            userInfo,
-          });
+  useEffect(() => {
+    try {
+      (async () => {
+        if (localStorage.getItem("auth")) {
+          const response = await fetchData("/Auth/Refresh");
+          loginUser(response.userId, response);
+          console.log("wel ingelogd:)");
+        } else {
+          console.log("niet ingelogd:)");
+        }
+      })();
+    } catch {
+      console.log("nog niet ingelogd");
     }
-    
-    
-    function logoutUser() {
-        setAuth({
-          authenticated: false,
-          userInfo: null,
-        });
-      }
+  }, []);
+
+  function loginUser(userId, userInfo) {
+    setAuth({
+      authenticated: true,
+      userInfo,
+    });
+    localStorage.setItem("auth", true);
+  }
+
+  function logoutUser() {
+    setAuth({
+      authenticated: false,
+      userInfo: null,
+    });
+    localStorage.setItem("auth", false);
+  }
 
   return (
-    <AuthContext.Provider value={{...auth, loginUser, logoutUser}}>
-        {children}
+    <AuthContext.Provider value={{ ...auth, loginUser, logoutUser }}>
+      {children}
     </AuthContext.Provider>
-  )
+  );
 }
-
--- src/pages/account/login/login.jsx
+++ src/pages/account/login/login.jsx
@@ -1,10 +1,40 @@
 import "@pagestyles/register.scss";
-
+import { fetchData } from "@api";
 import { Logo, LoadingDiv } from "@components";
 import LoginForm from "./components/form";
 import GoogleButton from "./components/GoogleButton";
-
+import { useAuth } from "@hooks";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 function Login() {
+  const { authenticated, loginUser } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    try {
+      (async () => {
+        console.log(localStorage.getItem("auth"));
+        if (localStorage.getItem("auth")) {
+          const response = await fetchData("/Auth/Refresh");
+          loginUser(response.userId, response);
+          navigate("/");
+          console.log("wel hier ook ingelogd:)");
+        } else {
+          console.log("niet ingelogd:)");
+        }
+      })();
+    } catch {
+      console.log("nog niet ingelogd");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (authenticated) {
+      navigate("/");
+    }
+    console.log("wordt gedaan");
+  }, []);
+
   return (
     <section id="register">
       <Logo id="logo"></Logo>

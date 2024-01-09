0b514655ea0f7d5e78d9f198d80097983b0d970e
Jason <jason256709@outlook.com>
2024-01-09 21:38:43 +0100

    Revert "check auth methode toegevoegd"
    This reverts commit 48ad9437ef9970add26ac244163b7097abf657f9.

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,46 +1,35 @@
-import { createContext, useState } from "react";
-import { ApiResponseError, fetchApi } from "@api";
+import { createContext, useState } from 'react'
+
 
 export const AuthContext = createContext();
 
-const checkAuth = async () => {
-  try {
-    const response = await fetchData("/Auth/Login");
-
-    setIsIngelogd(true);
-    navigate("/");
-    console.log(response);
-    loginUser(response.userId, response);
-  } catch (error) {
-    console.log("niet ingelogd");
-  }
-};
-
 const defaultAuth = {
-  authenticated: false,
-  userInfo: null,
-};
+    authenticated: false,
+    userInfo: null,
+}
 
-export function AuthProvider({ children }) {
-  const [auth, setAuth] = useState(defaultAuth);
+export function AuthProvider({children}) {
+    const [auth, setAuth] = useState(defaultAuth);
 
-  function loginUser(userId, userInfo) {
-    setAuth({
-      authenticated: true,
-      userInfo,
-    });
-  }
-
-  function logoutUser() {
-    setAuth({
-      authenticated: false,
-      userInfo: null,
-    });
-  }
+    function loginUser(userId, userInfo) {
+        setAuth({
+            authenticated: true,
+            userInfo,
+          });
+    }
+    
+    
+    function logoutUser() {
+        setAuth({
+          authenticated: false,
+          userInfo: null,
+        });
+      }
 
   return (
-    <AuthContext.Provider value={{ ...auth, loginUser, logoutUser }}>
-      {children}
+    <AuthContext.Provider value={{...auth, loginUser, logoutUser}}>
+        {children}
     </AuthContext.Provider>
-  );
+  )
 }
+

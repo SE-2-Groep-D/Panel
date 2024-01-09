9a9918e714891c8ee841539f03baedb2ae02db2b
David Out <david.out@migrato.nl>
2024-01-09 18:14:49 +0100

    Added loading animation to form.

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,13 +1,16 @@
-import {Button} from "@components";
+import {Button, LoadingDiv} from "@components";
 import PropTypes from 'prop-types';
+import {useState} from "react";
 
 const Form = ({title, message, children, buttonText, onSubmit, className}) => {
+    const [loading, setLoading] = useState(false);
 
-    function submitForm(event) {
+    async function submitForm(event) {
         event.preventDefault();
         if(onSubmit === undefined || onSubmit === null) return;
+
         const form = event.target;
-        const content = form.childNodes[1];
+        const content = form.childNodes[1].childNodes[0];
         const button = form.querySelector('button.form-button');
 
         const data = {
@@ -15,29 +18,35 @@ const Form = ({title, message, children, buttonText, onSubmit, className}) => {
         }
 
         content.childNodes.forEach((item, index) => {
-    
             const id = (item.getAttribute('id') === null || item.getAttribute('id') === undefined) ? index : item.getAttribute('id');
             data.values = {...data.values, [id] : item.getAttribute('value')}
         })
-        onSubmit(data);
+
+        setLoading(true);
+        await onSubmit(data);
+        setLoading(false)
     }
 
     const finalClassName = (className) ? 'form ' + className : 'form';
 
     return (
-        <form id={title + "-form"} className={finalClassName} onSubmit={submitForm}>
-            <div className='form-header'>
-                <h1 className='form-title heading-1'>{title}</h1>
-                <p className='form-message'>{message}</p>
-            </div>
-            <div className='form-content'>
-                {children}
-            </div>
+            <form id={title + "-form"} className={finalClassName} onSubmit={submitForm}>
+                <div className='form-header'>
+                    <h1 className='form-title heading-1'>{title}</h1>
+                    <p className='form-message'>{message}</p>
+                </div>
 
-            <div className='form-footer'>
-                <Button type='submit' className='form-button'>{buttonText}</Button>
-            </div>
-        </form>
+                <LoadingDiv loading={loading}>
+                    <div className='form-content'>
+                        {children}
+                    </div>
+
+                    <div className='form-footer'>
+                        <Button type='submit' className='form-button'>{buttonText}</Button>
+                    </div>
+                </LoadingDiv>
+            </form>
+
     );
 }
 

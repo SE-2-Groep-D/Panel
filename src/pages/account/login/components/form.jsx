d894b29a981433d460d9bd7dd37df5d0c1f3fd6f
Jason <jason256709@outlook.com>
2024-01-09 17:24:35 +0100

    register ook kleinere methode

--- src/pages/account/login/components/form.jsx
+++ src/pages/account/login/components/form.jsx
@@ -17,7 +17,7 @@ function LoginForm() {
     setGelukt(true);
   }
 
-  const handleSubmit2 = async () => {
+  const handleSubmit = async () => {
     try {
       const response = await fetchApi("/Auth/Login", "POST", {
         email: newUser.email,
@@ -30,39 +30,39 @@ function LoginForm() {
     }
   };
 
-  const handleSubmit = async () => {
-    try {
-      const response = await fetch("https://localhost:5000/Auth/Login", {
-        method: "POST",
-        credentials: "include",
-        body: JSON.stringify({
-          email: newUser.email,
-          password: newUser.password,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      });
+  // const handleSubmit = async () => {
+  //   try {
+  //     const response = await fetch("https://localhost:5000/Auth/Login", {
+  //       method: "POST",
+  //       credentials: "include",
+  //       body: JSON.stringify({
+  //         email: newUser.email,
+  //         password: newUser.password,
+  //       }),
+  //       headers: {
+  //         "Content-type": "application/json; charset=UTF-8",
+  //       },
+  //     });
 
-      let data = response.ok
-        ? await response.json()
-        : response.status === 400
-        ? await response.text()
-        : console.log("Login failed.");
-      console.log(data || "No data");
+  //     let data = response.ok
+  //       ? await response.json()
+  //       : response.status === 400
+  //       ? await response.text()
+  //       : console.log("Login failed.");
+  //     console.log(data || "No data");
 
-      response.ok
-        ? (setIsIngelogd(true), navigate("/"), loginUser(data.userId, data))
-        : setGelukt(false);
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
+  //     response.ok
+  //       ? (setIsIngelogd(true), navigate("/"), loginUser(data.userId, data))
+  //       : setGelukt(false);
+  //   } catch (error) {
+  //     console.error(error.message);
+  //   }
+  // };
 
   return (
     <>
       {!isIngelogd && (
-        <Form title="Inloggen" buttonText="Inloggen" onSubmit={handleSubmit2}>
+        <Form title="Inloggen" buttonText="Inloggen" onSubmit={handleSubmit}>
           <InputField
             id="email"
             type="email"
--- src/pages/account/setup/postRequests/registerBedrijf.js
+++ src/pages/account/setup/postRequests/registerBedrijf.js
@@ -1,13 +1,9 @@
 import {apiConfig} from '@api';
+import { fetchApi } from "@api";
 
 async function registerBedrijf(user) {
-  const hostname = (apiConfig.inDevelopment) ? apiConfig.development : apiConfig.production;
-    try {
-      console.log("ik word uitgevoerd");
-      const response = await fetch(hostname + "Auth/RegisterBedrijf", {
-        method: "POST",
-        body: JSON.stringify({
-          voornaam: user.firstName,
+  const data = {
+    voornaam: user.firstName,
           achternaam: user.lastName,
           googleAccount: false,
           email: user.email,
@@ -19,44 +15,74 @@ async function registerBedrijf(user) {
           nummer: user.number,
           websiteUrl: user.websiteUrl,
           omschrijving: user.description,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      });
-  
-      console.log(
-        JSON.stringify({
-            voornaam: user.firstName,
-            achternaam: user.lastName,
-            googleAccount: false,
-            email: user.email,
-            password: user.password,
-            bedrijfsnaam: user.name,
-            postcode: user.postcode,
-            plaats: user.place,
-            straat: user.street,
-            nummer: user.number,
-            websiteUrl: user.websiteUrl,
-            omschrijving: user.description,
-        })
-      );
-  
-      if (response.ok) {
-        const data = await response.text();
-        if (data) {
-          console.log(data);
-        } else {
-          console.log("Message missing in the response.");
-        }
-      } else {
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response.body);
-      }
-    } catch (error) {
-      console.error(error.message);
-    }
   }
+
+  try {
+    const response = await fetchApi("Auth/RegisterBedrijf", "POST", data);
+    return true;
+  } catch (error) {
+    console.error(error.message);
+    return false;
+  }
+};
+
+// async function registerBedrijf(user) {
+//   const hostname = (apiConfig.inDevelopment) ? apiConfig.development : apiConfig.production;
+//     try {
+//       console.log("ik word uitgevoerd");
+//       const response = await fetch(hostname + "Auth/RegisterBedrijf", {
+//         method: "POST",
+//         body: JSON.stringify({
+//           voornaam: user.firstName,
+//           achternaam: user.lastName,
+//           googleAccount: false,
+//           email: user.email,
+//           password: user.password,
+//           bedrijfsnaam: user.name,
+//           postcode: user.postcode,
+//           plaats: user.place,
+//           straat: user.street,
+//           nummer: user.number,
+//           websiteUrl: user.websiteUrl,
+//           omschrijving: user.description,
+//         }),
+//         headers: {
+//           "Content-type": "application/json; charset=UTF-8",
+//         },
+//       });
+  
+//       console.log(
+//         JSON.stringify({
+//             voornaam: user.firstName,
+//             achternaam: user.lastName,
+//             googleAccount: false,
+//             email: user.email,
+//             password: user.password,
+//             bedrijfsnaam: user.name,
+//             postcode: user.postcode,
+//             plaats: user.place,
+//             straat: user.street,
+//             nummer: user.number,
+//             websiteUrl: user.websiteUrl,
+//             omschrijving: user.description,
+//         })
+//       );
+  
+//       if (response.ok) {
+//         const data = await response.text();
+//         if (data) {
+//           console.log(data);
+//         } else {
+//           console.log("Message missing in the response.");
+//         }
+//       } else {
+//         console.log(response.status);
+//         console.log(response.statusText);
+//         console.log(response.body);
+//       }
+//     } catch (error) {
+//       console.error(error.message);
+//     }
+//   }
   
   export  {registerBedrijf};

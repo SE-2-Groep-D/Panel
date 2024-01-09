d4f1aa23ba19edc6c7beea288972e89fea273194
David Out <david.out@migrato.nl>
2024-01-09 16:58:46 +0100

    Added fetch api method.

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,31 @@
 import {getHostName} from '@api';
 
 async function fetchData(endpoint) {
+   return await fetchApi(endpoint, {}, "GET");
+}
+
+async function fetchApi(endpoint, data, method) {
     const hostname = getHostName();
+
     try {
-        const response = await fetch(hostname + endpoint);
+        const response = await fetch(hostname + endpoint, {
+            method: method,
+            credentials: "include",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        });
+
         if(!response.ok) {
-            console.log('Failed to fetch dat from: ', response.url);
+            console.log('Failed to communicate with: ', response.url);
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
-        return await response.json();
     } catch (error) {
         console.log('Error while fetching data: ', error);
         throw error;
     }
 }
 
-export {fetchData};
+
+export {fetchData, fetchApi};

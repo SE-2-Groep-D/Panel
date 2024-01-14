705236de7923f82b5bc928241d2cacef1c48c019
Jason <jason256709@outlook.com>
2024-01-14 10:47:39 +0100

    profiel pagina aan nav toegevoegd

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -1,83 +1,93 @@
-import {Link, useLocation} from "react-router-dom";
-import {Logo, Button} from "@components";
+import { Link, useLocation } from "react-router-dom";
+import { Logo, Button } from "@components";
 
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHome, faMagnifyingGlass, faNewspaper, faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
-import {useAuth} from "@hooks";
-import {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faMagnifyingGlass,
+  faNewspaper,
+  faRightFromBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "@hooks";
+import { useState } from "react";
 
-const hideNavigationRoutes  = [
-    '/setup',
-    '/login',
-    '/register',
-    '/privacy',
-    '/onderzoek/aanmaken',
-    '/onderzoek/:onderzoekId'
+const hideNavigationRoutes = [
+  "/setup",
+  "/login",
+  "/register",
+  "/privacy",
+  "/onderzoek/aanmaken",
+  "/onderzoek/:onderzoekId",
 ];
 
 function Navigation() {
-    const {authenticated, logoutUser, userInfo} = useAuth();
-    const route = useLocation();
+  const { authenticated, logoutUser, userInfo } = useAuth();
+  const route = useLocation();
 
-    if(hideNavigationRoutes.includes(route.pathname) || !authenticated) {
-        return null;
-    }
+  if (hideNavigationRoutes.includes(route.pathname) || !authenticated) {
+    return null;
+  }
 
-    const isActive = (path) => {
-        return location.pathname === path ? 'active' : '';
-    };
+  const isActive = (path) => {
+    return location.pathname === path ? "active" : "";
+  };
 
-    return <nav className='navigation'>
-        <input type={"checkbox"} id='navigation-checkbox' />
+  return (
+    <nav className="navigation">
+      <input type={"checkbox"} id="navigation-checkbox" />
 
-        <Link className='home' to='/'>
-            <Logo small/>
-        </Link>
+      <Link className="home" to="/">
+        <Logo small />
+      </Link>
 
-        <ul className="navigation-items">
-            <div className="links">
-                <li className={`navigation-item ${isActive('/')}`}>
-                    <Link to='/'>
-                        <FontAwesomeIcon icon={faHome} />
-                        Dashboard
-                    </Link>
-                </li>
-                <li className={`navigation-item ${isActive('/onderzoek')}`}>
-                    <Link to='/onderzoek'>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                        Onderzoeken
-                    </Link>
-                </li>
+      <ul className="navigation-items">
+        <div className="links">
+          <li className={`navigation-item ${isActive("/")}`}>
+            <Link to="/">
+              <FontAwesomeIcon icon={faHome} />
+              Dashboard
+            </Link>
+          </li>
+          <li className={`navigation-item ${isActive("/onderzoek")}`}>
+            <Link to="/onderzoek">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+              Onderzoeken
+            </Link>
+          </li>
 
-
-
-                {
-                    (userInfo.userType !== 'Bedrijf') ?
-                        <li className={`navigation-item ${isActive('/nieuwsbrief')}`}>
-                            <Link to='/nieuwsbrief'>
-                                <FontAwesomeIcon icon={faNewspaper} />
-                                Nieuws
-                            </Link>
-                        </li>
-                        : null
-                }
-            </div>
-
-            <Button aria-label={'Log uit.'} className='logout' onClick={() => logoutUser()}>
-                <FontAwesomeIcon icon={faRightFromBracket} />
-                Logout
-            </Button>
-        </ul>
-
-
-
-        <div className="navigation-button">
-            <label className='menu' htmlFor='navigation-checkbox'>
-                <span className="menu-icon"></span>
-            </label>
+          {userInfo.userType !== "Bedrijf" ? (
+            <li className={`navigation-item ${isActive("/nieuwsbrief")}`}>
+              <Link to="/nieuwsbrief">
+                <FontAwesomeIcon icon={faNewspaper} />
+                Nieuws
+              </Link>
+            </li>
+          ) : null}
         </div>
+        <li className={`navigation-item ${isActive("/profiel")}`}>
+          <Link to="/profiel">
+            <FontAwesomeIcon icon={faUser} />
+            Profiel
+          </Link>
+        </li>
+        <Button
+          aria-label={"Log uit."}
+          className="logout"
+          onClick={() => logoutUser()}
+        >
+          <FontAwesomeIcon icon={faRightFromBracket} />
+          Logout
+        </Button>
+      </ul>
 
+      <div className="navigation-button">
+        <label className="menu" htmlFor="navigation-checkbox">
+          <span className="menu-icon"></span>
+        </label>
+      </div>
     </nav>
+  );
 }
 
 export default Navigation;

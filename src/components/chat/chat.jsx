7d8bf3f0bfdae25286f01560a38b6a762194c34c
MaxHB01 <67209168+MaxHB01@users.noreply.github.com>
2024-01-18 22:22:31 +0100

    Added functionality

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -1,14 +1,35 @@
 import React, { useState } from 'react';
+import {useLocation} from "react-router-dom";
 import PopupChatWindow from './PopupChatWindow';
 import "@pagestyles/_chat.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMessage,
 } from "@fortawesome/free-solid-svg-icons";
+import {useAuth} from "@hooks";
+
+
+const hideNavigationRoutes  = [
+  '/setup',
+  '/login',
+  '/register',
+  '/privacy'
+];
+
 
 function Chat() {
+
+  const route = useLocation();
+  const {authenticated} = useAuth();
+
+  if(hideNavigationRoutes.includes(route.pathname) || !authenticated) {
+      return null;
+  }
+
+  const {userInfo} = useAuth();
   const [isChatOpen, setChatOpen] = useState(false);
-  const userId = '08dc15d5-8cec-4cb8-803e-efbe6a46efa9'; // Replace with actual logged-in user's ID
+  //const userId = '08dc185e-a40e-4f6a-85e2-05b1b7327e85'; // Replace with actual logged-in user's ID
+  const userId = userInfo.id; // Replace with actual logged-in user's ID
 
   const toggleChat = () => {
     setChatOpen(!isChatOpen);

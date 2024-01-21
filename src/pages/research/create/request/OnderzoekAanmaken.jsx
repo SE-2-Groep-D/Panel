7f0a19bab62bee24774b97a06d7dc87b692d3cad
Ozancann01 <22008306@student.hhs.nl>
2024-01-21 16:33:23 +0100

    tracking script toegevoegd

--- src/pages/research/create/request/OnderzoekAanmaken.jsx
+++ src/pages/research/create/request/OnderzoekAanmaken.jsx
@@ -13,13 +13,33 @@ async function OnderzoekAanmaken(onderzoek,user) {
         type:onderzoek.type,
         bedrijfId: user.id
     }
-
     try {
         const response = await fetchApi("/Onderzoek/create", "POST", data);
+        console.log(response)
+
+        if (onderzoek.type==="websiteBezoek"){
+
+            const trackingscriptdata={
+                onderzoekId: response.id,
+                domain: response.websiteUrl
+            }
+            try {
+                const response = await fetchApi("/Tracking/create", "POST", trackingscriptdata);
+                console.log(response)
+            }
+            catch (error){
+                return false
+            }
+
+        }
+
         return response;
+
     } catch (error) {
         return false;
     }
+
+
 }
 
 export  {OnderzoekAanmaken};
